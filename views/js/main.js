4f59710f3fa921ddb7caf0d2cb951f0dd48d4ed6
unknown <vartika.bhati@gmail.com>
2015-03-26 10:16:56 -0700

    incorporated review comments

--- views/js/main.js
+++ views/js/main.js
@@ -461,9 +461,10 @@ var resizePizzas = function(size) {
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
+// moved this line of code outside for loop
+var pizzasDiv = document.getElementById("randomPizzas");
 // This for-loop actually creates and appends all of the pizzas when the page loads
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
